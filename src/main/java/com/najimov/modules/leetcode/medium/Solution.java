3259197a387fbe3f59802128ecd01058018090c5
razorbilldev <najimovm11@gmail.com>
2025-11-21 12:33:12 +0500

    it was good I liked the problem but later I will try to optimize it

--- src/main/java/com/najimov/modules/leetcode/medium/Solution.java
+++ src/main/java/com/najimov/modules/leetcode/medium/Solution.java
@@ -1,6 +1,8 @@
 package com.najimov.modules.leetcode.medium;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 
 public class Solution {
@@ -66,6 +68,40 @@ public class Solution {
             right++;
         }
         return right - left;
+    }
+
+    public int longestSubarray(int[] nums) {
+        if (nums.length == 1) return 0;
+        List<Integer> list = new ArrayList<>();
+        int countOnes = 0, countZeros = 0;
+        int max = Integer.MIN_VALUE;
+        boolean status = false;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                countOnes++;
+                if (i == nums.length - 1) list.add(countOnes);
+            } else {
+                status = true;
+                if (countOnes != 0) {
+                    list.add(countOnes);
+                    countOnes = 0;
+                } else if (countZeros != 0) {
+                    list.add(0);
+                    countZeros = 0;
+                }
+                countZeros++;
+            }
+        }
+        if (list.size()==1) {
+            if (status) {
+                return list.get(0)-1;
+            }
+            return list.get(0);
+        }
+        for (int i = 0; i < list.size() - 1; i++) {
+            max = Math.max(list.get(i) + list.get(i + 1), max);
+        }
+        return max;
     }
 }
 
