0bc8185cb55f39e5691547703dbe0c5c9c39e062
razorbilldev <najimovm11@gmail.com>
2025-11-30 21:52:51 +0500

    resolved one leetcode esolution

--- src/main/java/com/najimov/modules/leetcode/easy/Solution.java
+++ src/main/java/com/najimov/modules/leetcode/easy/Solution.java
@@ -1,30 +1,55 @@
 package com.najimov.modules.leetcode.easy;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {1, 12, -5, -6, 50, 3};
-        int k = 4;
-        System.out.println(Solution.findMaxAverage(nums, k));
+        int[] nums1 = {1,2,3};
+        int[] nums2 = {2,4,6};
+        System.out.println(findDifference(nums1, nums2));
     }
 
     public static double findMaxAverage(int[] nums, int k) {
         int maxSum = Integer.MIN_VALUE;
-        int sum =0;
+        int sum = 0;
         //calulate the first window sum
-        for(int i=0;i<k;i++){
+        for (int i = 0; i < k; i++) {
             sum += nums[i];
         }
 
         maxSum = sum;
 
         //now loop over the remaining array ,using this window calculate sum and compare with maxSum
-        for(int i=k;i<nums.length;i++){
-            sum = sum - nums[i-k] + nums[i];
+        for (int i = k; i < nums.length; i++) {
+            sum = sum - nums[i - k] + nums[i];
             maxSum = Math.max(maxSum, sum);
         }
         return (double) maxSum / k;
+    }
+
+    public static List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        List<List<Integer>> result = new ArrayList<>();
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for (Integer num1 : nums1) {
+            for (Integer num2 : nums2) {
+                if (!num1.equals(num2)) {
+                    set1.add(num1);
+                }
+            }
+        }
+        for (Integer num2 : nums2) {
+            for (Integer num1 : nums1) {
+                if (!num2.equals(num1)) {
+                    set2.add(num2);
+                }
+            }
+        }
+        result.add(new ArrayList<>(set1));
+        result.add(new ArrayList<>(set2));
+        return result;
     }
 }
