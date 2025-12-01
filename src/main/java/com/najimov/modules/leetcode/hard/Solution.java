cc7d4082ec1c8502603b9f76c7d8c046a0dff2da
razorbilldev <najimovm11@gmail.com>
2025-12-01 14:27:57 +0500

    solved hard level task

--- src/main/java/com/najimov/modules/leetcode/hard/Solution.java
+++ src/main/java/com/najimov/modules/leetcode/hard/Solution.java
@@ -1,7 +1,35 @@
 package com.najimov.modules.leetcode.hard;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
+        int n = 2;
+        int[] batteries = {3, 3, 3};
+        System.out.println(maxRunTime(n, batteries));
+    }
 
+    public static long maxRunTime(int n, int[] batteries) {
+        long sum=0;
+        for (int b: batteries) sum += b;
+        long left = 0, right = sum / n;
+        while (left < right) {
+            long mid = (left + right + 1) / 2;
+            if (canRun(mid, n, batteries)) {
+                left = mid;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    private static boolean canRun(long t, int n, int[] batteries) {
+        long total=0;
+        for (int b : batteries) {
+            total += Math.min(b, t);
+
+        }
+        return total >= n * t;
     }
 }
--- src/main/java/com/najimov/modules/leetcode/medium/Solution.java
+++ src/main/java/com/najimov/modules/leetcode/medium/Solution.java
@@ -1,15 +1,11 @@
 package com.najimov.modules.leetcode.medium;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1};
-        System.out.println(solution.longestOnesSecondSolution(nums, 0));
+        System.out.println(solution.closeStrings("cabbba", "abbccc"));
     }
 
     public int maxVowels(String s, int k) {
@@ -92,9 +88,9 @@ public class Solution {
                 countZeros++;
             }
         }
-        if (list.size()==1) {
+        if (list.size() == 1) {
             if (status) {
-                return list.get(0)-1;
+                return list.get(0) - 1;
             }
             return list.get(0);
         }
@@ -102,6 +98,21 @@ public class Solution {
             max = Math.max(list.get(i) + list.get(i + 1), max);
         }
         return max;
+    }
+
+    public boolean closeStrings(String word1, String word2) {
+        if (word1.length() != word2.length()) return false;
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        for (char c : word1.toCharArray()) map1.put(c, map1.getOrDefault(c, 0) + 1);
+        for (char c : word2.toCharArray()) map2.put(c, map2.getOrDefault(c, 0) + 1);
+        if (!map1.keySet().equals(map2.keySet())) return false;
+
+        List<Integer> list1 = new ArrayList<>(map1.values());
+        List<Integer> list2 = new ArrayList<>(map2.values());
+        Collections.sort(list1);
+        Collections.sort(list2);
+        return list1.equals(list2);
     }
 }
 
